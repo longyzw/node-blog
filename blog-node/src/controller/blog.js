aaa299dcaf47b21d588bcc9575349cc29bcc30b0
龙影之舞 <752490640@qq.com>
2020-01-12 15:09:56 +0800

    模拟博客文章的数据返回

--- blog-node/src/controller/blog.js
+++ blog-node/src/controller/blog.js
@@ -1,3 +1,4 @@
+// 获取文章列表
 const getList = query => {
     // 获取请求参数
     const author = query.author || ''
@@ -24,6 +25,65 @@ const getList = query => {
     }
 }
 
+// 获取文章详情
+const getDetail = query => {
+    // 获取请求参数
+    const id = query.id || ''
+    // 先返回假数据
+    return {
+        code: '0000',
+        data: [
+            {
+                id: 1,
+                author: 'a',
+                title: 'A',
+                content: 'aaa',
+                createTime: 1578752491648
+            }
+        ]
+    }
+}
+
+// 添加文章
+const addBlog = body => {
+    // 获取请求参数
+    const title = body.title || ''
+    const content = body.content || ''
+    // 先返回假数据
+    return {
+        code: '0000',
+        data: '添加成功'
+    }
+}
+
+// 删除文章
+const delBlog = body => {
+    // 获取请求参数
+    const id = body.id || ''
+    // 先返回假数据
+    return {
+        code: '0000',
+        data: '删除成功'
+    }
+}
+
+// 修改文章
+const getUpdate = body => {
+    // 获取请求参数
+    const id = body.id || ''
+    const title = body.title || ''
+    const content = body.content || ''
+    // 先返回假数据
+    return {
+        code: '0000',
+        data: '更新成功'
+    }
+}
+
 module.exports = {
-    getList
+    getList,
+    getDetail,
+    addBlog,
+    delBlog,
+    getUpdate
 }
