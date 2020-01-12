1f3f7688faa2a2f16fbf790d8137c118ad70f521
龙影之舞 <752490640@qq.com>
2020-01-12 15:11:13 +0800

    用户数据返回的拆分处理

--- blog-node/src/router/user.js
+++ blog-node/src/router/user.js
@@ -1,15 +1,20 @@
+const { login } = require('../controller/user')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+
 const handleUserRouter = (req, res) => {
     // 接收方法、路径、参数
-    const { method, path, query } = req
+    const { method, path, query, body } = req
 
     if (method === 'POST') {
         // 用户登录
-        if (path === '/api/user/login') {
-            return {
-                msg: '登录'
-            }
-        }
+        if (path === '/api/user/login') return getResult(login(body))
     }
+}
+
+// 处理查询返回结果
+const getResult = res => {
+    let { code, data }  = res
+    return code === 0 ? new SuccessModel(code, data) : new ErrorModel(code, data)
 }
 
 module.exports = handleUserRouter
