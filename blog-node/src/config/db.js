821d7b6ed1cd19598c75333cb890e8adc4cea5f4
龙影之舞 <752490640@qq.com>
2020-01-12 18:55:59 +0800

    数据库连接配置

---
+++ blog-node/src/config/db.js
@@ -0,0 +1,26 @@
+const env = process.env.NODE_ENV // 环境变量
+
+// 配置
+const MYSQL_CONFIG
+
+if (env === 'dev') {
+    MYSQL_CONFIG = {
+        host: 'localhost',
+        user: 'root',
+        password: 'lyzw',
+        port: '3306',
+        database: 'blog-node'
+    }
+}
+
+if (env === 'production') {
+    MYSQL_CONFIG = {
+        host: 'localhost',
+        user: 'root',
+        password: 'lyzw',
+        port: '3306',
+        database: 'blog-node'
+    }
+}
+
+module.exports = MYSQL_CONFIG
